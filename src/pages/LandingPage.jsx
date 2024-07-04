f26dfb4fa61976a2a9f1ddcfda262536ae875ccd
Densanity <denniszixiang@gmail.com>
2024-07-04 18:18:36 +0200

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,17 +1,26 @@
 export default function LandingPage() {
   return (
-    <div>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <div className="card-body">
-            <h2 className="card-title">Hello, DaisyUI!</h2>
-            <p>Welcome to your new project with Tailwind CSS and DaisyUI.</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Get Started</button>
-            </div>
+    <>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1719733250222-d7f51c02a4ce?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
