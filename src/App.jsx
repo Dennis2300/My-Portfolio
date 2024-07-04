5c1ab5a052f8d7290437ef68554d9bf2172d67e5
Densanity <denniszixiang@gmail.com>
2024-07-04 18:07:53 +0200

    Landing Page 1.0 Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+// Components
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-
   return (
-    <>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <div className="card-body">
-          <h2 className="card-title">Hello, DaisyUI!</h2>
-          <p>Welcome to your new project with Tailwind CSS and DaisyUI.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
-    </div>
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,17 @@
+export default function LandingPage() {
+  return (
+    <div>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Hello, DaisyUI!</h2>
+            <p>Welcome to your new project with Tailwind CSS and DaisyUI.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
